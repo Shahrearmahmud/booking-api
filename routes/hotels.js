85fb8a45d29bbac18552726ed1a5c533dd77138a
Shahrear mahmud <aodriteosrk10252gmail.com>
2022-09-14 21:58:57 +0600

    1sthalf

---
+++ routes/hotels.js
@@ -0,0 +1,26 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+const router = express.Router();
+
+
+
+//create
+
+router.post("/",async (req,res)=>{
+
+    const newHotel = new Hotel(req.body)
+    try{
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+//update
+//get
+//det all
+
+export default router
